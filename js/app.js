39a89b36c897255879a87a34ce64aeaa9f016927
paolasalazar <paola.s.satoritech@gmail.com>
2026-06-09 08:32:31 -0600

    animacion 1

--- js/app.js
+++ js/app.js
@@ -1,27 +1,45 @@
+const intro =
+document.getElementById("intro");
+
+const startBtn =
+document.getElementById("startBtn");
+
+const actions =
+document.getElementById("actions");
+
+startBtn.addEventListener("click", () => {
+
+intro.style.display = "none";
+
+document
+.getElementById("ar-container")
+.style.display = "block";
+
+});
+
 window.addEventListener("DOMContentLoaded", () => {
 
-    const websiteBtn =
-    document.querySelector("#websiteBtn");
-    
-    const whatsappBtn =
-    document.querySelector("#whatsappBtn");
-    
-    websiteBtn.addEventListener("click", () => {
-    
-    window.open(
-    "https://www.imas.agency",
-    "_blank"
-    );
-    
-    });
-    
-    whatsappBtn.addEventListener("click", () => {
-    
-    window.open(
-    "https://wa.me/524461846064",
-    "_blank"
-    );
-    
-    });
-    
-    });
+const target =
+document.querySelector("#target");
+
+const image =
+document.querySelector("#brandImage");
+
+target.addEventListener("targetFound", () => {
+
+image.setAttribute(
+"animation",
+"property: scale; to: 1 1 1; dur: 700; easing: easeOutBack"
+);
+
+actions.style.display = "flex";
+
+});
+
+target.addEventListener("targetLost", () => {
+
+actions.style.display = "none";
+
+});
+
+});
