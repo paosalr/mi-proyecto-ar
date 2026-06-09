f4c8a1f13b160cb2f4a4218947f8b64336e25c0b
paolasalazar <paola.s.satoritech@gmail.com>
2026-06-09 08:41:39 -0600

    animacion 2

--- js/app.js
+++ js/app.js
@@ -27,10 +27,27 @@ document.querySelector("#brandImage");
 
 target.addEventListener("targetFound", () => {
 
+image.removeAttribute("animation");
+
+image.setAttribute(
+"scale",
+"0.2 0.2 0.2"
+);
+
+setTimeout(() => {
+
 image.setAttribute(
 "animation",
-"property: scale; to: 1 1 1; dur: 700; easing: easeOutBack"
+`
+property: scale;
+from: 0.2 0.2 0.2;
+to: 1 1 1;
+dur: 800;
+easing: easeOutElastic;
+`
 );
+
+}, 50);
 
 actions.style.display = "flex";
 
